83606590190719e3a67055d500f7e7057a7a5736
Kalziea <Kalziea25@gmail.com>
2022-10-05 09:39:03 -0600

    Created most of the DB sql names

---
+++ backend/database/users.js
@@ -0,0 +1,19 @@
+//Create User
+const createUser({  })
+
+//Get User
+const getUserByUserId
+
+const getUserByUsername
+
+//Get All Users
+const getAllUsers
+
+//Edit User
+const editUser(id, edit stuff)
+
+//Delete User
+const deleteUser(id)
+
+//Admin Update Users for making another person an Admin.
+const updateAdmin

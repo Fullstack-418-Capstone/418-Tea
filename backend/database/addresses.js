446446f1c57ead38c967ea79555663f5f9bfcc80
Richard <richardmassengale@yaho.com>
2022-10-05 11:14:04 -0600

    create 'createAddress' function

--- backend/database/addresses.js
+++ backend/database/addresses.js
@@ -1,8 +1,23 @@
-//Edit Address
+const client = require('./client')
+
+//Create Address
+const createAddress = async(address) => {
+    const {address1, address2, city, state, zipcode} = address
+    try {
+        const {rows: {id} } = await client.query(`
+            INSERT INTO addresses (address1, address2, city, state, zipcode)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING id
+        `, [address1, address2 ? address2 : null, city, state, zipcode ])
+
+        return id
+    } catch (error) {
+        throw error
+    }
+}
 
 //Get Address
 
-//Create Address
-const createAddress
+//Edit Address
 
 //Delete Address

8e4f34214f041b3a01ac9e0c713cb8ffa1bb9516
Megan Donaldson <meganmaried123@gmail.com>
2022-10-06 09:30:24 -0500

    front end tingz

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 // import React from "react";
-import {createRoot} from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 
 const App = () => {
-    console.log('test')
+  const BASE_URL = "http://localhost:3000/api";
 
+  return <h1>What's the tea</h1>;
+};
 
-    return (<h1>What's the tea</h1>)
-}
-
-
-const container = document.getElementById('app')
-const root = createRoot(container)
-root.render(<App />)
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);

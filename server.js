0e8f22cfacd7b22d2d37346a1e8076c051a3172d
Fred Davis <F092495@gmail.com>
2022-10-10 11:41:23 -0400

    working API connection

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const server = express();
 const PORT = 3001;
 
+require("dotenv").config();
+// const express = require("express")
+const cors = require('cors');
+server.use(express.json())
+
+server.use(cors());
+
 const apiRouter = require("./backend/api");
 server.use("/api", apiRouter);
 
-// const { client } = require("./backend/db");
-// client.connect();
+const  client  = require("./backend/database/client.js");
+client.connect();
 
 server.listen(PORT, () => {
   console.log("The server is up on port:", PORT);

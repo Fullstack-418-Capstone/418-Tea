ec943d1e2f1f78e0516ddd8a50ee059da01d027d
Fred Davis <F092495@gmail.com>
2022-10-10 15:30:46 -0400

    filters added to teaLeaf

--- src/components/TeaLeaf.js
+++ src/components/TeaLeaf.js
@@ -1,43 +1,47 @@
 import React,{useEffect, useState} from 'react';
 import { getAllActiveProducts } from '../api';
+import FilterButton from './FilterButtonView';
 import ProductWindow from './ProductWindow';
 
 const TeaLeaf = () => {
     const [tea, setTea] = useState([])
-    const [filter, setFilter] = useState(["loose", "bagged"])
+    const [filterWord, setFilterWord] = useState(["loose", "bagged"])
     const [allActiveProducts, setAllActiveProducts] = useState([]);
 
-
-    const filterProducts = () => {
-        const filtered = [];
-        console.log("all active products", allActiveProducts)
-        for(let i = 0; i < allActiveProducts.length; i++ ){
-            if(filter.includes(allActiveProducts[i].type)){
-                filtered.push(allActiveProducts[i])
-            }
-        }
-        console.log('adding to', filtered)
-        setTea(filtered)
-    }
 
     const getAll = async() => {
         const allproduct = await getAllActiveProducts();
         console.log('fetched products', allproduct)
         setAllActiveProducts(allproduct)
-
+        filterProducts(allproduct)
     }
+    const filterProducts = (productArr) => {
+        const newArr=[]
+        for(let i = 0; i< productArr.length; i++){
+            if(filterWord.includes(productArr[i].type)){
+                console.log("matching", productArr[i])
+                newArr.push(productArr[i])
+            }
+        }
+        setTea(newArr)
+    }
+
 
     useEffect(() => {
         getAll();
-        setFilter(["loose", "bagged"])
     },[])
     useEffect(() => {
-        filterProducts();
-    }, [filter])
+        filterProducts(allActiveProducts)
+    },[filterWord])
 
 
     return(
         <div>
+
+            <FilterButton text='tea' setFilterWord={setFilterWord}></FilterButton>
+            
+            <FilterButton text='loose' setFilterWord={setFilterWord}></FilterButton>
+            <FilterButton text='bagged' setFilterWord={setFilterWord}></FilterButton>
             {tea[0] ?
             tea.map((product,index) => {
                 return (

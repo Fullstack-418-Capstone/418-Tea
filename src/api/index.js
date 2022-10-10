2c83f8d33e55bc84f86462c25c68a8c56b0306f1
Megan Donaldson <meganmaried123@gmail.com>
2022-10-10 14:41:14 -0500

    register feature

--- src/api/index.js
+++ src/api/index.js
@@ -1,117 +1,125 @@
 // for all API call functions
 const BASE_URL = "http://localhost:3001/api";
 
-const loginUser = async(username, password, setToken, setUser, setIsAdmin) => {
+const loginUser = async (username, password, setToken, setUser, setIsAdmin) => {
+  let success;
 
-    let success;
+  try {
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const data = await response.json();
 
-    try {
-        const response = await fetch(`${BASE_URL}/users/login`, {
-            method: "POST",
-            headers: {
-            'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username,
-                password      
-            })
-        })
-        const data = await response.json()
-
-        if(data.token) {
-            setToken(data.token)
-            setUser(username)
-            setIsAdmin(data.isAdmin)
-            localStorage.setItem('418WhatsTeaToken', data.token)
-            localStorage.setItem('418WhatsTeaUser', JSON.stringify(data.user))
-            success = true
-        } else {
-            success=false
-        }
-        
-        alert(data.message)
-        return success
-    } catch (error) {
-        console.error(error)
+    if (data.token) {
+      setToken(data.token);
+      setUser(username);
+      setIsAdmin(data.isAdmin);
+      localStorage.setItem("418WhatsTeaToken", data.token);
+      localStorage.setItem("418WhatsTeaUser", JSON.stringify(data.user));
+      success = true;
+    } else {
+      success = false;
     }
-}
 
-const getAllProducts = async() => {
-    try{
-        const response = await fetch(`${BASE_URL}/products`,{
-            method:'GET',
-            headers: {
-                'Content-Type': 'application/json'
-                },
-        });
-        const data = await response.json();
-        return data
-    } catch (error) {
-        console.error(error)
-    }
-}
-const getAllActiveProducts = async() => {
-    try{
-        const response = await fetch(`${BASE_URL}/products`,{
-            method:'GET',
-            headers: {
-                'Content-Type': 'application/json'
-                },
-        });
-        const data = await response.json();
-        return data
-    } catch (error) {
-        console.error(error)
-    }
-}
-const getAllUsers = async() => {
-    try{
-        const response = await fetch(`${BASE_URL}/users`,{
-            method:'GET',
-            headers: {
-                'Content-Type': 'application/json'
-                },
-        });
-        const result = await response.json();
-        return result
-    }catch (error) {
-        console.error(error)
-    }
-}
-const registerUser = async (firstname, lastname, username, password, email, addressObj,) => {
-    try{
-        const response = await fetch(`${BASE_URL}/users/register`,{
-            method:'POST',
-            headers: {
-                'Content-Type': 'application/json'
-                },
-            body: JSON.stringify({
-                firstname: firstname,
-                lastname: lastname,
-                username: username,
-                password: password,
-                email: email,
-                address: addressObj
-            })                
-        });
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.error(error)
-    }
-}
+    alert(data.message);
+    return success;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-
-
-
-
-
-
+const getAllProducts = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/products`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+const getAllActiveProducts = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/products`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+const getAllUsers = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/users`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+const registerUser = async (
+  firstname,
+  lastname,
+  username,
+  password,
+  email,
+  address1,
+  address2,
+  city,
+  state,
+  zipcode
+) => {
+  try {
+    console.log("STATEEEE", state);
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstname: firstname,
+        lastname: lastname,
+        username: username,
+        password: password,
+        email: email,
+        address1,
+        //address2,
+        city,
+        state,
+        zipcode,
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export {
-    getAllProducts,
-    getAllActiveProducts,
-    registerUser,
-    getAllUsers,
-    loginUser
-}
+  getAllProducts,
+  getAllActiveProducts,
+  registerUser,
+  getAllUsers,
+  loginUser,
+};

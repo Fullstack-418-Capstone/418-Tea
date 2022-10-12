7518c9cf2f07795e90f7c4872e4c860ba8a3dfcd
Megan Donaldson <meganmaried123@gmail.com>
2022-10-12 13:06:37 -0500

    i am so sorry fred

--- backend/database/orders.js
+++ backend/database/orders.js
@@ -1,153 +1,194 @@
-const client = require("./client")
+const client = require("./client");
 
 //Create Order
 const createOrder = async (userId) => {
-    try {
-        const { rows: [newOrder] } = await client.query(`
+  try {
+    const {
+      rows: [newOrder],
+    } = await client.query(
+      `
         INSERT INTO orders("userId")
         VALUES ($1)
         RETURNING *;
-        `, [userId]);
+        `,
+      [userId]
+    );
 
-        return newOrder;
-    } catch(err) {
-        console.error(err);
-        throw err;
-    }
+    return newOrder;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
 //Edit Order by Order ID
 const editOrderByOrderId = async (orderId, ...fields) => {
-    const setString = Object.keys(fields).map(
-        (key, index) => `"${key}" = $${ index + 1}`
-    ).join(', ');
-    try {
-        const { rows: [editedOrder] } = await client.query(`
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}" = $${index + 1}`)
+    .join(", ");
+  try {
+    const {
+      rows: [editedOrder],
+    } = await client.query(
+      `
         UPDATE orders
         SET ${setString}
         WHERE "orderId" = ${orderId}
         RETURNING *;
-        `, Object.values(fields));
-        
-        return editedOrder;
-    } catch(err) {
-        console.error(err);
-        throw err;
-    }
-}
+        `,
+      Object.values(fields)
+    );
+
+    return editedOrder;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
 //Get All Orders
 const getAllOrders = async () => {
-    try{
-        const { rows: allOrders } = await client.query(`
+  try {
+    const { rows: allOrders } = await client.query(`
         SELECT *
         FROM orders;
         `);
 
-        return allOrders
-    } catch(err) {
-        console.error(err);
-        throw err;
-    }
-
-}
+    return allOrders;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
 //Get Open Orders
 const getOpenOrders = async () => {
-    try{
-        const { rows: openOrders } = await client.query(`
+  try {
+    const { rows: openOrders } = await client.query(`
         SELECT *
         FROM orders
         WHERE "isOpen" = true;
         `);
-    
-        return openOrders
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
-}
+
+    return openOrders;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
 //Get Orders by UserId (Open and Closed)
 const getOrdersbyUserId = async (userId) => {
-    try{
-        const { rows: orders } = await client.query(`
+  try {
+    const { rows: orders } = await client.query(
+      `
         SELECT *
         FROM orders
         WHERE "userId" = $1;
-        `, [userId]);
+        `,
+      [userId]
+    );
 
-        return orders;
-    } catch(error) {
-        throw error
-    }
-}
+    return orders;
+  } catch (error) {
+    throw error;
+  }
+};
 
 //Get "Cart" by UserId (The active order)
 const getOpenCartByUser = async (userId) => {
-    try {
-        const { rows: openCart } = await client.query(`
+  try {
+    const { rows: openCart } = await client.query(
+      `
         SELECT id
         FROM orders
         WHERE "userId" = $1 AND "isOpen" = true;
-        `, [userId]);
+        `,
+      [userId]
+    );
 
-        if(openCart.length === 0 ) {
-            const cartId = await createOrder(userId);
-            return cartId;
-        } else {
-            return openCart[0];
-        }
-    } catch(err) {
-        console.error(err);
-        throw(err);
+    if (openCart.length === 0) {
+      const cartId = await createOrder(userId);
+      return cartId;
+    } else {
+      return openCart[0];
     }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
 //Delete Order
 const deleteOrder = async (orderId) => {
-    try{
-        const { rows: [deletedOrder] } = await client.query(`
+  try {
+    const {
+      rows: [deletedOrder],
+    } = await client.query(
+      `
         DELETE FROM orders
         WHERE id = $1
         RETURNING *;
-        `, [orderId]);
+        `,
+      [orderId]
+    );
 
-        return deletedOrder;
-    } catch(err) {
-        console.error(err);
-        throw err;
-    }
+    return deletedOrder;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
-const placeOrder = async(orderId) => {
-   // const setString = `"isOpen=$1, "status"=$2`
-    
-    try{
-        const { rows: [order] } = await client.query(`
+const placeOrder = async (orderId) => {
+  // const setString = `"isOpen=$1, "status"=$2`
+
+  try {
+    const {
+      rows: [order],
+    } = await client.query(`
         UPDATE orders
         SET "isOpen"=false, "status"='ordered'
         WHERE id=${orderId}
         RETURNING *;
         `);
-        
-        return order
 
-    } catch(err) {
-        console.error(err);
-        throw err;
-    }
-}
+    return order;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
-//Get Order by Status? 
+const getOrdersByOrderId = async (orderId) => {
+  try {
+    const {
+      rows: [order],
+    } = await client.query(
+      `
+          SELECT *
+          FROM orders
+          WHERE "id" = $1;
+          `,
+      [orderId]
+    );
+
+    return order;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//Get Order by Status?
 
 module.exports = {
-    createOrder,
-    editOrderByOrderId,
-    getAllOrders,
-    getOpenOrders,
-    getOrdersbyUserId,
-    getOpenCartByUser,
-    deleteOrder,
-    placeOrder
+  createOrder,
+  editOrderByOrderId,
+  getAllOrders,
+  getOpenOrders,
+  getOrdersbyUserId,
+  getOpenCartByUser,
+  deleteOrder,
+  placeOrder,
+  getOrdersByOrderId,
 };
--- src/index.js
+++ src/index.js
@@ -1,66 +1,73 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import {
-    Header,
-    HomePage,
-    Register,
-    TeaLeaf,
-    TeaWare,
-    Cart,
-    AdminPage,
-    Profile
-} from './components'
+  Header,
+  HomePage,
+  Register,
+  TeaLeaf,
+  TeaWare,
+  Cart,
+  AdminPage,
+  Profile,
+} from "./components";
 
 const App = () => {
-  const [isAdmin, setIsAdmin] = useState(false)
-  const [token, setToken] = useState('')
-  const [user, setUser] = useState({})
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState({});
 
   useEffect(() => {
-    if(localStorage.getItem('418WhatsTeaToken') && !token) {
-        const localUser = JSON.parse(localStorage.getItem('418WhatsTeaUser'))
-        setToken(localStorage.getItem('418WhatsTeaToken'))
-        setUser(localUser)
-        setIsAdmin(localUser.isAdmin)
+    if (localStorage.getItem("418WhatsTeaToken") && !token) {
+      const localUser = JSON.parse(localStorage.getItem("418WhatsTeaUser"));
+      setToken(localStorage.getItem("418WhatsTeaToken"));
+      setUser(localUser);
+      setIsAdmin(localUser.isAdmin);
     }
-  }, [])
+  }, []);
 
-  useEffect(() => {}, [token])
+  useEffect(() => {}, [token]);
 
-return (
-  <Router>
-    <div>
+  return (
+    <Router>
+      <div>
         <header>
-            <Header token={token} isAdmin={isAdmin} setToken={setToken} setUser={setUser} setIsAdmin={setIsAdmin} />
+          <Header
+            token={token}
+            isAdmin={isAdmin}
+            setToken={setToken}
+            setUser={setUser}
+            setIsAdmin={setIsAdmin}
+          />
         </header>
-        
+
         <main>
-            <Routes>
-              <Route path='/register' element = {
-                <Register setToken={setToken}></Register>
-              }></Route>
-              <Route path='/tea-leaf' element = {
-                <TeaLeaf></TeaLeaf>
-              }></Route>
-              <Route path='/tea-ware' element = {
-                <TeaWare></TeaWare>
-              }></Route>
-              <Route path='/cart' element = {
-                <Cart token={token} user={user} ></Cart>
-              }></Route>
-              <Route path='/profile' element = {
-                <Profile token={token} ></Profile>
-              }></Route>
-              <Route path='/admin' element = {
-                <AdminPage isAdmin={isAdmin}></AdminPage>
-              }></Route>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
+          <Routes>
+            <Route
+              path="/register"
+              element={<Register setToken={setToken}></Register>}
+            ></Route>
+            <Route path="/tea-leaf" element={<TeaLeaf></TeaLeaf>}></Route>
+            <Route path="/tea-ware" element={<TeaWare></TeaWare>}></Route>
+            <Route
+              path="/cart"
+              element={<Cart token={token} user={user}></Cart>}
+            ></Route>
+            <Route
+              path="/profile"
+              element={<Profile token={token} user={user}></Profile>}
+            ></Route>
+            <Route
+              path="/admin"
+              element={<AdminPage isAdmin={isAdmin}></AdminPage>}
+            ></Route>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
         </main>
-    </div>
-  </Router>);
+      </div>
+    </Router>
+  );
 };
 
 const container = document.getElementById("app");

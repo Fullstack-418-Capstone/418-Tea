dd75307895683826316af8dadb963265a4be7aa7
Richard <richardmassengale@yaho.com>
2022-10-13 11:23:22 -0600

    changed previous orders to be numbered based off user orders, instead of orderId

--- src/components/PastOrderView.js
+++ src/components/PastOrderView.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PastOrderLine from "./PastOrderProductsLine";
 
-const PastOrderView = ({ order }) => {
+const PastOrderView = ({ order, index }) => {
   return (
     <div>
-      <h3> Order #{order[0].orderId}</h3>
+      <h3> Order #{index + 1}</h3>
       {order.map((item, i) => {
         return <PastOrderLine key={i} item={item}></PastOrderLine>;
       })}

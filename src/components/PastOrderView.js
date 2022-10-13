c348f31408394a655f64598d8cd86679583549aa
Megan Donaldson <meganmaried123@gmail.com>
2022-10-13 10:50:28 -0500

    orders are styled

--- src/components/PastOrderView.js
+++ src/components/PastOrderView.js
@@ -2,7 +2,6 @@ import React from "react";
 import PastOrderLine from "./PastOrderProductsLine";
 
 const PastOrderView = ({ order }) => {
-  console.log("ORDER HERE", order);
   return (
     <div>
       <h3> Order #{order[0].orderId}</h3>

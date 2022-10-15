a3c53a34743044c82e587a68b88ac7b5d0291ced
Richard <richardmassengale@yaho.com>
2022-10-14 18:25:03 -0600

    heroku test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = express();
-const {PORT = 3001} = process.env;
+const PORT = process.env.PORT || 3001;
 
 require("dotenv").config();
 const cors = require('cors');
